89b8eadab3eed354b075d9252935360a224467d1
Josh Steubs <joshua.steubs@gmail.com>
2021-09-07 01:24:03 -0700

    museum styling,formatting, instructions

--- javascript/virtual-museum/pages/BaselineForm.js
+++ javascript/virtual-museum/pages/BaselineForm.js
@@ -1,16 +1,25 @@
+import SubmitSurveyButton from "./SubmitSurveyButon"
 export default function BaseineForm() {
     return (
         <>
             <div>
                 <div className="md:grid md:grid-cols-2 md:gap-6">
+
                     <div className="mt-5 md:mt-0 md:col-span-2">
                         <form action="#" method="POST">
                             <div className="shadow sm:rounded-md sm:overflow-hidden">
+
                                 <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
+                                    <h1 className="block text-lg font-medium text-gray-700">Survey #1</h1>
+                                    <div className="hidden sm:block" aria-hidden="true">
+                                        <div className="py-2">
+                                            <div className="border-t border-gray-300" />
+                                        </div>
+                                    </div>
                                     <div className="grid grid-cols-3 gap-6">
                                         <div className="col-span-3 sm:col-span-2">
                                             <label htmlFor="id-num" className="block text-md font-medium text-gray-700">
-                                                Q1.1 Participant ID #
+                                                Q1.1 Participant ID # (NOTE FROM JOSH: Is this being kept in?)
                                             </label>
                                             <div className="mt-1 ">
 
@@ -602,7 +611,11 @@ export default function BaseineForm() {
                                                         More than 3 times
                                                     </label>
                                                 </div>
-
+                                                <div className="hidden sm:block" aria-hidden="true">
+                                                    <div className="py-5">
+                                                        <div className="border-t border-gray-200" />
+                                                    </div>
+                                                </div>
                                             </div>
                                         </fieldset>
                                     </div>
@@ -656,17 +669,9 @@ export default function BaseineForm() {
                                             />
                                         </div>
                                     </div>
-
+                                    <p className='p-2 text-sm bg-red-700'>TODO FOR JOSH: BASELINE QUESTIONS ARE INCOMPLETE </p>
                                 </div>
 
-                                <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                                    <button
-                                        type="submit"
-                                        className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                                    >
-                                        Submit
-                                    </button>
-                                </div>
                             </div>
                         </form>
                     </div>
