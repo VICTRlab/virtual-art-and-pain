0c687f7a9fa5ee8f410d53d2d18f59d47efb5c2e
Josh Steubs <joshua.steubs@gmail.com>
2021-09-13 20:45:29 -0700

    last prer err fix

--- javascript/virtual-museum/pages/RadioQuestion.js
+++ javascript/virtual-museum/pages/RadioQuestion.js
@@ -26,7 +26,7 @@ export default class RadioQuestion extends Component {
 
                     <div className="mt-4 space-y-4">
                         <fieldset>
-                            {options.map((option) => (
+                            {this.props.options && options.map((option) => (
                                 <div className="flex items-center" key={option.text}>
                                     <input
 
