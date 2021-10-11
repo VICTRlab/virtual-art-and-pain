f3bcfb20eb6604f075db5f2112dec50d42a14b74
steubs <joshua.steubs@gmail.com>
2021-10-10 23:10:07 -0700

    add mus text,timer

--- unity/VICTR-LAB-Museum/Assets/Scripts/GalleryManager.cs
+++ unity/VICTR-LAB-Museum/Assets/Scripts/GalleryManager.cs
@@ -8,7 +8,9 @@ public class GalleryManager : MonoBehaviour
     public GameObject welcomePanelObj;
     public GameObject completeTextObj;
     
-
+    public GameObject t1;
+    public GameObject t2;
+    public bool t1On = false;
     private double timer = 0.0;
     public bool startTimer = true;
     public GameObject timeTextObj;
@@ -19,12 +21,17 @@ public class GalleryManager : MonoBehaviour
         listNum = -1;
         welcomeTextObj = GameObject.Find("Welcome");
         welcomePanelObj = GameObject.Find("Panel");
-        completeTextObj = GameObject.Find("CompleteText");
-        completeTextObj.SetActive(false);
+        //completeTextObj = GameObject.Find("CompleteText");
+        //completeTextObj.SetActive(false);
 
         timeTextObj = GameObject.Find("TimeText");
         timeText = timeTextObj.GetComponent<Text>();
         timeTextObj.SetActive(false);
+
+        t1 = GameObject.Find("t1");
+        t2 = GameObject.Find("t2");
+        t1.SetActive(false);
+        t2.SetActive(false);
     }
     void Update(){
         if(listNum==-1) {
@@ -36,6 +43,18 @@ public class GalleryManager : MonoBehaviour
             }
         }
         if(listNum==0) {
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                if(t1On){
+                    t1.SetActive(false);
+                    t2.SetActive(false);
+                    t1On = !t1On;
+                } else {
+                    t1.SetActive(true);
+                    t2.SetActive(true);
+                    t1On = !t1On;
+                }
+            }
             if(startTimer) {
                 timer = 600.0;
                 startTimer=false;
@@ -48,5 +67,6 @@ public class GalleryManager : MonoBehaviour
             }
             
         }
+        
     }
 }
