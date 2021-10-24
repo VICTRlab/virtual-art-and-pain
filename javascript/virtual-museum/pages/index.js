e54b12459239a1566ca3670d57975b30cb672446
Adam Ali <adam@dijla.io>
2021-10-24 12:56:28 -0700

    Update index.js

--- javascript/virtual-museum/pages/index.js
+++ javascript/virtual-museum/pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import { useEffect } from 'react'
 import styles from '../styles/Home.module.css'
 import App from './App'
 import { initializeApp } from "firebase/app";
